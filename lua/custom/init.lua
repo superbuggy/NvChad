c679d6858173e73db68316d9bca41502410b0dca
superbuggy <jpqreichard@gmail.com>
2023-11-20 00:22:24 -0500

    🙈 Include customizations

---
+++ lua/custom/init.lua
@@ -0,0 +1,10 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+-- vim.o.updatetime = 250
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
